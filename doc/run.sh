201890b93425aba4ef7b8971267749ac41d17c3b
Mohammad <akhlaghi@astr.tohoku.ac.jp>
2014-04-14 20:27:11 +0900

    Manual not finished yet, getting there...

--- doc/run.sh
+++ doc/run.sh
@@ -1,2 +1,8 @@
 texi2pdf mockgals.texinfo
 rm *.aux *.cp *.fn *.ky *.log *.pg *.toc *.tp *.vr
+texi2any mockgals.texinfo
+texi2any --docbook mockgals.texinfo
+texi2any --plaintext -o mockgals.txt mockgals.texinfo
+
+rm -rf ./mockgals 
+texi2any --html --split=chapter mockgals.texinfo
